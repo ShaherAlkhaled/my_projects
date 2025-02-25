49d48bbf7bdc826d012d15525cfc28c7f409156a
ShaherAlkhaled <alkhaledshaher@outlook.com>
2025-02-25 18:31:47 +0100

    update api project vue.js

--- Api_project/src/Imi.Project.Vue/obj/Debug/net5.0/Imi.Project.Vue.RazorTargetAssemblyInfo.cs
+++ Api_project/src/Imi.Project.Vue/obj/Debug/net5.0/Imi.Project.Vue.RazorTargetAssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyProductAttribute("Imi.Project.Vue")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11960a20394d7a471282ce71ac246c6c42ce65d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+077763db0b6dee6bb2c4bd3a4b0ec183a98a6589")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Imi.Project.Vue.Views")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
